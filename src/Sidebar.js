745ac2f26654bf98c3cc011f22fa111e85714856
Kushagra <kushagra198@gmail.com>
2021-03-28 18:24:07 +0530

    SideBarOption Updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,43 @@
-import React from 'react'
-import "./Sidebar";
-import "./Sidebar.css"
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import React from "react";
+import "./Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from '@material-ui/icons/Create';
+ import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Sidebar() {
-    return <div className="sidebar">
-            <div className="sidebar__header">
-            <div className="sidebar__info">
-            <h2>slack-clone</h2>
-                <h3>
-               <FiberManualRecordIcon/>
-                  Abc
-                </h3>
-            </div>
-            <CreateIcon />
-            </div>
-        </div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <div className="sidebar__info">
+          <h2>slack-clone</h2>
+          <h3>
+            <FiberManualRecordIcon />
+            Kushagra
+          </h3>
+        </div>
+        <CreateIcon />
+      </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Threads" />
+      <SidebarOption Icon={InboxIcon} title="Mention & reaction" />
+      <SidebarOption Icon={DraftsIcon} title="Saved item" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <SidebarOption Icon={ExpandMoreIcon} title="Show more" />
+      <hr />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
